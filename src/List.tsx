f2b691f41f88fae80aa71d3d0ad290384b154292
Hashib Raja <hashibraja23@gmail.com>
2023-09-26 20:36:29 +0530

    Created list component 📃📃

---
+++ src/List.tsx
@@ -0,0 +1,26 @@
+import { MovieI } from "./App";
+
+type Props = {
+  movies: MovieI;
+};
+
+const List = ({ movies }: Props) => {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <li key={movie.imdbID}>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>🗓</span>
+              <span>{movie.Year}</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
