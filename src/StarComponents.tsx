c2eba89b9601144ee9afdb71239aa04c0f982552
Hashib Raja <hashibraja23@gmail.com>
2023-09-27 08:12:04 +0530

    implemented mouse hover and leave functionality 🌿🌿

--- src/StarComponents.tsx
+++ src/StarComponents.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 type PropsS = {
+  onLeave: () => void;
+  onHover: () => void;
   onClick: () => void;
   full: boolean;
 };
 
-const Star = ({ onClick, full }: PropsS) => {
+const Star = ({ onClick, full, onHover, onLeave }: PropsS) => {
   return full ? (
-    <span onClick={onClick} style={{ marginRight: "10px", cursor: "pointer" }}>
+    <span
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      onClick={onClick}
+      style={{ marginRight: "10px", cursor: "pointer" }}
+    >
       ⭐
     </span>
   ) : (
-    <span onClick={onClick} style={{ marginRight: "10px", cursor: "pointer" }}>
+    <span
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      onClick={onClick}
+      style={{ marginRight: "10px", cursor: "pointer" }}
+    >
       ✡️
     </span>
   );
@@ -22,16 +34,19 @@ type Props = {
 
 const StarComponents = ({ max }: Props) => {
   const [numStar, setNumStar] = useState(0);
+  const [tempStar, setTempStar] = useState(0);
   return (
     <div>
       {Array.from({ length: max }, (_, i) => (
         <Star
           key={i}
           onClick={() => setNumStar(i + 1)}
-          full={numStar >= i + 1}
+          full={tempStar ? tempStar >= i + 1 : numStar >= i + 1}
+          onHover={() => setTempStar(i + 1)}
+          onLeave={() => setTempStar(0)}
         />
       ))}
-      <span>{numStar || ""}</span>
+      <span>{tempStar || numStar || ""}</span>
     </div>
   );
 };
