172f4e1712666b607f1ebc73445fbd2ff88fe349
Hashib Raja <hashibraja23@gmail.com>
2023-10-01 09:23:05 +0530

    added watched movie in local storage and read data in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -21,62 +21,20 @@ export interface WatchMovieI extends MovieI {
   userRating: number;
 }
 
-// const tempMovieData: MovieI[] = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt0133093",
-//     Title: "The Matrix",
-//     Year: "1999",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt6751668",
-//     Title: "Parasite",
-//     Year: "2019",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-//   },
-// ];
-
-// const tempWatchedData: WatchMovieI[] = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//     runtime: 148,
-//     imdbRating: 8.8,
-//     userRating: 10,
-//   },
-//   {
-//     imdbID: "tt0088763",
-//     Title: "Back to the Future",
-//     Year: "1985",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-//     runtime: 116,
-//     imdbRating: 8.5,
-//     userRating: 9,
-//   },
-// ];
-
 export const KEY = "2b099c57";
 
 export default function App() {
   const [query, setQuery] = useState("india");
   const [movies, setMovies] = useState<MovieI[]>([]);
-  const [watched, setWatched] = useState<WatchMovieI[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [selectId, setSelectId] = useState<string | null>(null);
   const [error, setError] = useState("");
+
+  const [watched, setWatched] = useState<WatchMovieI[]>(() => {
+    const data = localStorage.getItem("watched");
+    if (!data) return [];
+    return JSON.parse(data);
+  });
 
   const handleSelect = (id: string) => {
     setSelectId((prev) => (prev === id ? null : id));
@@ -86,6 +44,14 @@ export default function App() {
 
   const handleAddWatchedMovie = (movie: WatchMovieI) => {
     setWatched((prev) => [...prev, movie]);
+  };
+
+  const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleDeleteWatched = (id: string) => {
+    setWatched((prev) => prev.filter((ele) => ele.imdbID !== id));
   };
 
   useEffect(() => {
@@ -128,13 +94,9 @@ export default function App() {
     };
   }, [query]);
 
-  const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-  };
-
-  const handleDeleteWatched = (id: string) => {
-    setWatched((prev) => prev.filter((ele) => ele.imdbID !== id));
-  };
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   return (
     <>
