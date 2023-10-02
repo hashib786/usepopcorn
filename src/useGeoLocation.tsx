79d2c7f58f448a601072a7925dd615c77cd85bbe
Hashib Raja <hashibraja23@gmail.com>
2023-10-02 07:51:26 +0530

    Added starter of useGeolocation

---
+++ src/useGeoLocation.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+// function useGeolocation() {}
+
+type position = {
+  lat?: number;
+  lng?: number;
+};
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState<position>({});
+  const [error, setError] = useState<string | null>(null);
+
+  const { lat, lng } = position;
+
+  function getPosition() {
+    setCountClicks((count) => count + 1);
+
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={getPosition} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
